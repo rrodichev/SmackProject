b83193693dcf89cce0974d611d336adab82f7940
Roza Rodichev <rodichevqa@gmail.com>
2019-11-26 17:24:22 -0800

    setup initial proj

--- README.md
+++
@@ -1,2 +0,0 @@
-# SmackProject
-ios app
---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Rozalia Rodichev on 11/26/19.
+//  Copyright © 2019 Rozalia Rodichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
